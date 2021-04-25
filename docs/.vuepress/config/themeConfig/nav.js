d20d2edc93530542ee398dbdbae766866ec636de
yudioll <sunyuliang@aixuexi.com>
2021-04-25 21:17:47 +0800

    yudioll: 更改目录设置

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -1,4 +1,8 @@
 // nav
 module.exports = [
-  { text: '首页', link: '/' }
+  { text: '首页', link: '/' },
+  {text: 'JavaScript', link: '/categories/?category=front'},
+  {text: 'React', link: '/categories/?category=react'},
+  {text: 'webpack', link: '/categories/?category=webpack'},
+  {text: '网络基础', link: '/categories/?category=network'},
 ]
